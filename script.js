0ae90a509d5dd4667435b0958cefc3ea1d8bb2ee
gauyex <gauyex@bzz.ch>
2023-10-05 23:50:19 +0200

    remove task button hat "child security switch" oder so drin. man muss sich ja schliesslich 2 mal überlegen, ob man den task wirklich für immer löschen will. Vielleicht clickt man ja aus versehen auf complete task und weiss dann nicht mehr, was da stand. Auf einmal verpasst man dann ein meeting mit dem Präsidenten der usa, was diesen dann nicht glücklich macht.

--- script.js
+++ script.js
@@ -1,12 +1,37 @@
-const tasklist = []
-const taskOutput = document.getElementById('taskOutput');
-document.forms.userinput.addEventListener("submit", function (event){
-    event.preventDefault();
+document.addEventListener('DOMContentLoaded', function () {
+    const tasklist = [];
+    const taskOutput = document.getElementById('taskOutput');
+    document.forms.userinput.addEventListener("submit", function (event) {
+        event.preventDefault();
+        const data = new FormData(event.target)
 
+        let newdiv = document.createElement("div")
+        let newP = document.createElement("p")
+        let newBtn = document.createElement("button")
+        let newInput = document.createElement("input")
+        newInput.setAttribute("type","checkbox")
+        newInput.setAttribute("name","checkbox")
+        newInput.setAttribute("id","checkbox")
+        newdiv.classList.add('task')
+        newBtn.setAttribute('disabled', 'true')
 
-    const data = new FormData(event.target)
-    console.log(data)
+        newP.innerText = data.get('task')
+        newBtn.innerText = 'Remove Task'
 
-    taskOutput.innerText = data.get('task')
-    document.getElementById('taskOutput').innerHTML = data.get('task')
-})
+        newBtn.onclick = () => {
+            newdiv.remove()
+        }
+
+        newInput.onchange = (event) => {
+            if (event.target.value){
+                newBtn.removeAttribute('disabled')
+                newInput.setAttribute("disabled","")
+            }
+        }
+
+        newdiv.appendChild(newP)
+        newdiv.appendChild(newInput)
+        newdiv.appendChild(newBtn)
+        taskOutput.appendChild(newdiv)
+    });
+});
