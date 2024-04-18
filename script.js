8cfb168a3d2cc7ff6081df08c2359a8ee1fdb5e1
sondereggern@bzz.ch <sondereggern@bzz.ch>
2024-04-18 15:58:54 +0200

    updated -> Local Storage funktioniert Jetzt :)

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+function addEventListeners(newP, newEdit, newBtn, newdiv, taskOutput) {
+    newEdit.onclick = () => {
+        let newTask = window.prompt("Change");
+        if (newTask !== null) {
+            newP.innerText = newTask;
+            // Update the localStorage when a task is edited
+            localStorage.setItem('entries', taskOutput.innerHTML);
+        }
+    }
+
+    newBtn.onclick = () => {
+        newdiv.remove();
+        // Delay the update to ensure that the innerHTML is updated
+        setTimeout(() => {
+            // Remove the task from localStorage when it's deleted
+            localStorage.setItem('entries', taskOutput.innerHTML);
+        }, 0);
+    }
+}
+
+window.onload = function () {
+    const taskOutput = document.getElementById('taskOutput');
+    if (localStorage.getItem('entries')) {
+        taskOutput.innerHTML = localStorage.getItem('entries');
+        // Add event listeners to existing tasks
+        Array.from(taskOutput.getElementsByClassName('task')).forEach(task => {
+            const newP = task.getElementsByTagName('p')[0];
+            const newEdit = task.getElementsByTagName('button')[0];
+            const newBtn = task.getElementsByTagName('button')[1];
+            addEventListeners(newP, newEdit, newBtn, task, taskOutput);
+        });
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function () {
     const taskOutput = document.getElementById('taskOutput');
     document.forms.userinput.addEventListener("submit", function (event) {
@@ -19,21 +53,15 @@ document.addEventListener('DOMContentLoaded', function () {
         newEdit.innerText = 'Edit Task'
         newBtn.innerText = 'Delete Task'
 
-        newEdit.onclick = () => {
-            let newTask = window.prompt("Change");
-            if (newTask !== null) {
-                newP.innerText = newTask;
-            }
-        }
-
-        newBtn.onclick = () => {
-            newdiv.remove()
-        }
+        addEventListeners(newP, newEdit, newBtn, newdiv, taskOutput);
 
         newdiv.appendChild(newP)
         newdiv.appendChild(newInput)
         newdiv.appendChild(newEdit)
         newdiv.appendChild(newBtn)
         taskOutput.appendChild(newdiv)
+
+        // Save the new task to localStorage
+        localStorage.setItem('entries', taskOutput.innerHTML);
     });
 });
